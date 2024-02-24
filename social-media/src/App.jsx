58e7fe9722d9173ab8d113887b9750dd00d3476f
Khan Shabaz Umar hayat <82037567+khanshabaz@users.noreply.github.com>
2024-02-24 08:44:40 +0530

    Initial Commit

---
+++ social-media/src/App.jsx
@@ -0,0 +1,30 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import PostListProvider from "./store/Post-list-store";
+
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <PostListProvider>
+      <div className="app-container">
+        <SideBar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></SideBar>
+        <div className="content">
+          <Header></Header>
+          <Outlet />
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListProvider>
+  );
+}
+
+export default App;
